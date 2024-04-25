071a8b45d99902fc929c095218eb436a45fec6fe
gpeduarte <gpe.duartecampus.fct.unl.pt>
2024-04-25 23:27:22 +0100

    Patch RefValue

--- src/value/RefValue.java
+++ src/value/RefValue.java
@@ -1,19 +1,19 @@
-//package value;
-//
-//public class RefValue implements Value{
-//
-//    private Value val;
-//
-//    public RefValue(Value val) {
-//        this.val = val;
-//    }
-//
-//    public void setVal(Value val){
-//        this.val = val;
-//    }
-//
-//    public Value getVal(){
-//        return this.val;
-//    }
-//
-//}
+package value;
+
+public class RefValue implements Value{
+
+    private Value val;
+
+    public RefValue(Value val) {
+        this.val = val;
+    }
+
+    public void setValue(Value val){
+        this.val = val;
+    }
+
+    public Value getValue(){
+        return this.val;
+    }
+
+}
