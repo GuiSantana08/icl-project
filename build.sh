acc21cc52fc59ee623851d41ac3e24bcba79de0c
gpeduarte <gpe.duarte@campus.fct.unl.pt>
2024-04-26 23:59:38 +0100

    Patch builder

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cd ../../ || exit
 
 javac src/ast/*.java src/ast/control/*.java src/ast/functions/*.java src/ast/operations/arithmetic/*.java src/ast/operations/references/*.java src/ast/operations/relational/*.java src/ast/value/*.java src/exceptions/*.java src/interpreter/Interpreter.java src/main/InterMain.java src/parser/*.java src/symbols/*.java src/type/*.java src/value/*.java
 
-jar cvf project.jar src/ast/*.class src/ast/control/*.class src/ast/functions/*.class src/ast/operations/arithmetic/*.class src/ast/operations/references/*.class src/ast/operations/relational/*.class src/ast/value/*.class src/exceptions/*.class src/interpreter/Interpreter.class src/main/InterMain.class src/parser/*.class src/symbols/*.class src/type/*.class src/value/*.class
+jar cmvf META-INF/MANIFEST.MF project.jar src/ast/*.class src/ast/control/*.class src/ast/functions/*.class src/ast/operations/arithmetic/*.class src/ast/operations/references/*.class src/ast/operations/relational/*.class src/ast/value/*.class src/exceptions/*.class src/interpreter/Interpreter.class src/main/InterMain.class src/parser/*.class src/symbols/*.class src/type/*.class src/value/*.class
 
 rm src/ast/*.class src/ast/control/*.class src/ast/functions/*.class src/ast/operations/arithmetic/*.class src/ast/operations/references/*.class src/ast/operations/relational/*.class src/ast/value/*.class src/exceptions/*.class src/interpreter/Interpreter.class src/main/InterMain.class src/parser/*.class src/symbols/*.class src/type/*.class src/value/*.class
