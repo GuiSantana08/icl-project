63f66706f867126769defa2387732d9997318e3f
gpeduarte <gpe.duarte@campus.fct.unl.pt>
2024-04-27 01:56:58 +0100

    Builder continua com o mesmo erro...

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ javacc -c Parser.jj
 
 cd ../../ || exit
 
-javac src/ast/*.java src/ast/control/*.java src/ast/functions/*.java src/ast/operations/arithmetic/*.java src/ast/operations/references/*.java src/ast/operations/relational/*.java src/ast/value/*.java src/exceptions/*.java src/interpreter/Interpreter.java src/main/InterMain.java src/parser/*.java src/symbols/*.java src/type/*.java src/value/*.java
+javac -d build src/ast/*.java src/ast/control/*.java src/ast/functions/*.java src/ast/operations/arithmetic/*.java src/ast/operations/references/*.java src/ast/operations/relational/*.java src/ast/value/*.java src/exceptions/*.java src/interpreter/Interpreter.java src/main/InterMain.java src/parser/*.java src/symbols/*.java src/type/*.java src/value/*.java
 
-jar cmvf META-INF/MANIFEST.MF project.jar src/ast/*.class src/ast/control/*.class src/ast/functions/*.class src/ast/operations/arithmetic/*.class src/ast/operations/references/*.class src/ast/operations/relational/*.class src/ast/value/*.class src/exceptions/*.class src/interpreter/Interpreter.class src/main/InterMain.class src/parser/*.class src/symbols/*.class src/type/*.class src/value/*.class
-
-rm src/ast/*.class src/ast/control/*.class src/ast/functions/*.class src/ast/operations/arithmetic/*.class src/ast/operations/references/*.class src/ast/operations/relational/*.class src/ast/value/*.class src/exceptions/*.class src/interpreter/Interpreter.class src/main/InterMain.class src/parser/*.class src/symbols/*.class src/type/*.class src/value/*.class
+jar cfm project.jar build/Manifest.txt build/ast/*.class build/ast/control/*.class build/ast/functions/*.class build/ast/operations/arithmetic/*.class build/ast/operations/references/*.class build/ast/operations/relational/*.class build/ast/value/*.class build/exceptions/*.class build/interpreter/Interpreter.class build/main/InterMain.class build/parser/*.class build/symbols/*.class build/type/*.class build/value/*.class
